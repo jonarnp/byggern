e0d8d44409baa4a7da27ec8f63a174d27ed11d87
unknown <boerikse@SANNTID08.ad.ime.ntnu.no>
2014-09-03 13:00:13 +0200

    finished exercise 2 (xmem and gal)

--- byggern_25/byggern_25/byggern_25.c
+++ byggern_25/byggern_25/byggern_25.c
@@ -6,7 +6,7 @@
  */ 
 
 #define F_CPU 4915200UL  // 4.9152 MHz
-#define BAUD 9600
+#define BAUD 38400
 #define MYUBRR F_CPU/16/BAUD-1
 
 #include <avr/io.h>
@@ -14,14 +14,32 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#include "bit_op.h"
 #include "drivers/usart.h"
 
-#define toggle_bit(reg,bit) (reg^= (1 << bit))
+#include "tests/sram_test.h"
 
 int main(void)
 {
 	USART_Init(MYUBRR);
+	XMEM_En();
 	DDRB = 0x01;
+	
+	//SRAM test counting errors
+	//uint8_t errors[512];
+	//calloc(512,sizeof(uint8_t));
+	//uint16_t i;
+	//for(i = 0; i < 10; i++){
+		//SRAM_test_count(errors);
+	//}
+	//for(i = 0; i < 512; i++){
+		//if(errors[i]!= 0) printf("addr: %d, numErrors: %d\n", i, errors[i]);
+	//}
+	//
+	
+	//SRAM test
+	SRAM_test();
+	
 	while(1)
 	{
 		////TODO:: Please write your application code
@@ -32,5 +50,7 @@ int main(void)
 			printf("Received data: %c\n", USART_Receive());
 		}
 		//USART_Transmit(USART_Receive());
+		
+		_delay_ms(1000);
 	}
 }
